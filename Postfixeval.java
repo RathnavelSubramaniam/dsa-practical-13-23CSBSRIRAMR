6213b2b5b45dd1fd0256a5e59aa8bab40769bf94
23CSBSRIRAMR <140476578+23CSBSRIRAMR@users.noreply.github.com>
2025-02-25 10:39:12 +0000

    p13

--- Postfixeval.java
+++ Postfixeval.java
@@ -1,103 +1,110 @@
 import java.io.*;
 import java.util.*;
-class stack 
-{
+
+class stack {
     int size;
     int item[];
     int top;
-    public stack() 
-    {
+
+    public stack() {
         size = 100;
         item = new int[size];
         top = -1;
     }
-    public void push(int ele) 
-    {
-        if (top == (size - 1)) 
-        {
+
+    public void push(int ele) {
+        if (top == (size - 1)) {
             System.out.println("Stack Overflow");
-        } else 
-        {
+        } else {
             top++;
             item[top] = ele;
-          //  System.out.println("Inserted Element:"+ele);
         }
     }
-    public int pop() 
-    {
-        if (top == -1) 
-        {
-            System.out.println("Invalid Postfix string; Operators are given more than operands");
-            return (-1);
-        } else 
-        {
+
+    public int pop() {
+        if (top == -1) {
+            System.out.println("No Elements");
+            return -1;
+        } else {
             int x = item[top];
             top--;
-          //  System.out.println("Popped Element:"+x);
-            return (x);
+            return x;
         }
     }
-    public int peek() 
-    {
-        if (top == -1) 
-        {
+
+    public int peek() {
+        if (top == -1) {
             System.out.println("No Elements");
-            return (-1);
-        } else
-            return (item[top]);
+            return -1;
+        } else {
+            return item[top];
+        }
     }
-    public void display() 
-    {
+
+    public void display() {
         System.out.println();
-        if (top == -1) 
-        {
+        if (top == -1) {
             System.out.println("No Elements");
-        } else 
-        {
+        } else {
             System.out.println("Stack is");
             for (int i = 0; i <= top; i++)
                 System.out.println(item[i]);
         }
     }
-    public boolean isEmpty()
-    {
-        if(top==-1)
-        return true;
-        else
-        return false;
-    }
 }
-class evalpostfix 
-{
+
+class evalpostfix {
     stack st = new stack();
     String postfix;
-    public evalpostfix(String str) 
-    {
+
+    public evalpostfix(String str) {
         postfix = str;
     }
-    public boolean isOperand(char ch) 
-    {
-     
-    // TYPE YOUR CODE CODE
-       
+
+    public boolean isOperand(char ch) {
+        return Character.isDigit(ch); // Checks if the character is a digit
     }
-    public int eval() 
-    {
-         // TYPE YOUR CODE CODE
-        
-     }
-public class Postfixeval 
-{
-    public static void main(String[] args) 
-    {
-        Scanner sc= new Scanner(System.in);
+
+    public int eval() {
+        for (int i = 0; i < postfix.length(); i++) {
+            char ch = postfix.charAt(i);
+
+            if (isOperand(ch)) {
+                st.push(ch - '0'); // Convert char digit to integer
+            } else {
+                int val2 = st.pop();
+                int val1 = st.pop();
+
+                switch (ch) {
+                    case '+':
+                        st.push(val1 + val2);
+                        break;
+                    case '-':
+                        st.push(val1 - val2);
+                        break;
+                    case '*':
+                        st.push(val1 * val2);
+                        break;
+                    case '/':
+                        st.push(val1 / val2);
+                        break;
+                    default:
+                        System.out.println("Invalid operator");
+                        return -1;
+                }
+            }
+        }
+        return st.pop(); // Final result
+    }
+}
+
+public class Postfixeval {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter postfix string");
-        String str=sc.nextLine();
+        String str = sc.nextLine();
         evalpostfix epf = new evalpostfix(str);
-        int res=epf.eval();
-        if(res!=-1)
-        System.out.println("Result:" +res);
-       else
-       System.out.println("Invlalid Postfix String");
+        System.out.println("Result:" + epf.eval());
+        sc.close();
     }
 }
